625d8088c871c5e2d5b91ccc1cfc3921882a495b
richar <ricardosiri68@gmail.com>
2013-10-29 01:41:35 -0300

    font properties necesitaba especificar a que expX debia adaptarse

--- ocr/train.sh
+++ ocr/train.sh
@@ -5,7 +5,7 @@ if test $# -ne 1;then
 fi
 tesseract spa.patentes.exp$1.tiff spa.patentes.exp$1.box nobatch box.train
 unicharset_extractor spa.patentes.exp$1.box
-echo "patentes 0 1 1 0 0" > spa.font_properties
+echo "patentes.exp$1 0 1 1 0 0" > spa.font_properties
 mftraining -F spa.font_properties -U unicharset -O spa.unicharset spa.patentes.exp$1.box.tr
 cntraining spa.patentes.exp$1.box.tr
 mv Microfeat spa.Microfeat
