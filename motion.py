17bf114e6349e5ac7cd6785b4bfae9e0a7e10a8e
richar <ricardosiri68@gmail.com>
2013-12-05 21:03:57 -0300

    quitando comentarios del codigo

--- motion.py
+++ motion.py
@@ -19,22 +19,3 @@ class Motion:
 		self.__frames += 1
 		if self.__frames == 15:
 			self.__prevImg = img
-
-
-# from SimpleCV import *
-
-# cam = Camera()
-# threshold = 5.0 # if mean exceeds this amount do something
-
-# while True:
-#     previous = cam.getImage() #grab a frame
-#     time.sleep(0.5) #wait for half a second
-#     current = cam.getImage() #grab another frame
-#     diff = current - previous
-#     matrix = diff.getNumpy()
-#     mean = matrix.mean()
-
-#     diff.show()
-
-#     if mean >= threshold:
-#             print "Motion Detected"
