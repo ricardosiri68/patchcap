94a25426f3cf97c72400e1f463bd82e0f7850616
richar <ricardosiri68@gmail.com>
2014-04-02 17:28:30 -0300

    corrigiendo pep8 en stats y outputstream

--- stats.py
+++ stats.py
@@ -3,6 +3,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class PatchStat(object):
 
     MAX_FAILURES = 100
@@ -25,19 +26,29 @@ class PatchStat(object):
         self._failures += 1
         if self._failures == 1:
             logger.error("No se pudo obtener la imagen")
-        if self._failures > PatchStat.MAX_FAILURES: 
-            logger.fatal("No se pueden obtener imagenes (repetido %s veces)"%(self.MAX_FAILURES))
+        if self._failures > PatchStat.MAX_FAILURES:
+            logger.fatal(
+                "No se pueden obtener imagenes (repetido %s veces)" %
+                (self.MAX_FAILURES)
+            )
             self._failures = 0
         time.sleep(1)
+
     def found(self):
-        self._found +=1
+        self._found += 1
 
     def detected(self):
-        self._detected +=1    
-    
+        self._detected += 1
+
     def count(self):
-        self._total +=1
+        self._total += 1
 
     def show(self):
-        logger.info("Detectadas correctamente %d/%d", self._found, self._detected)
-        logger.info("Se encontraron %d errores al procesar %d imagenes", self._failures,self._total)
+        logger.info(
+            "Detectadas correctamente %d/%d", self._found, self._detected
+        )
+        logger.info(
+            "Se encontraron %d errores al procesar %d imagenes",
+            self._failures,
+            self._total
+        )
