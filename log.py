85d93213c1e34b734ad881b8a20668b304987aa5
richar <ricardosiri68@gmail.com>
2014-04-02 17:24:33 -0300

    corrigiendo pep8 en log

--- log.py
+++ log.py
@@ -1,16 +1,25 @@
-import logging, logging.handlers
+import logging
+import logging.handlers
 import math
 import uuid
 from os import path, mkdir
+
+
 def setup():
 
-    formatter = logging.Formatter(fmt='%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+    format = '%(asctime)s - %(module)s - %(levelname)s - %(message)s'
+    formatter = logging.Formatter(fmt=format)
     console_formatter = logging.Formatter(fmt='%(message)s')
     console = logging.StreamHandler()
     console.setFormatter(console_formatter)
     console.setLevel(logging.DEBUG)
 
-    rotating_file = logging.handlers.RotatingFileHandler("logs/patchcap.log", mode = 'a', maxBytes = 10*math.pow(1024,3), backupCount=45)
+    rotating_file = logging.handlers.RotatingFileHandler(
+        "logs/patchcap.log",
+        mode='a',
+        maxBytes=10 * math.pow(1024, 3),
+        backupCount=45
+    )
     rotating_file.setFormatter(formatter)
     rotating_file.setLevel(logging.WARNING)
 
@@ -21,29 +30,30 @@ def setup():
 
     return logger
 
-def save_image(img, name =''):
+
+def save_image(img, name=''):
 
     filename = None
-    images_path =  "blobsChars/" 
+    images_path = "blobsChars/"
     if not path.isdir(images_path):
         mkdir(images_path)
 
     if img.filename:
         filename = path.splitext(path.basename(img.filename))[0].upper()
-        images_path += filename 
+        images_path += filename
         if not path.isdir(images_path):
             mkdir(images_path)
 
     if not name:
-        if filename is None:        
-            filename = str(uuid.uuid4()) 
+        if filename is None:
+            filename = str(uuid.uuid4())
         name = filename
-    
-    imgpath = path.join(images_path,"%s.png"%(name))
+
+    imgpath = path.join(images_path, "%s.png" % (name))
     if path.isfile(imgpath):
         i = 1
-        imgpath = path.join(images_path,"%s-%s.png"%(name,i))
+        imgpath = path.join(images_path, "%s-%s.png" % (name, i))
         while path.isfile(imgpath):
             i += 1
-            imgpath = path.join(images_path,"%s-%s.png"%(name,i))
-    img.save(imgpath) 
+            imgpath = path.join(images_path, "%s-%s.png" % (name, i))
+    img.save(imgpath)
