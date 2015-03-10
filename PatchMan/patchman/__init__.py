87cce12e700e08045bf3eb1f699ec55e6e137b15
Hernando Rojas <hernando.rojas@gmail.com>
2015-03-10 18:13:56 -0300

    move mako include to config

--- PatchMan/patchman/__init__.py
+++ PatchMan/patchman/__init__.py
@@ -1,13 +1,12 @@
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 from patchman.models import initialize_sql
-from patchman.utils.subscribers import add_renderer_globals
-from patchman.utils.subscribers import csrf_validation
 from patchman.utils.routes import MyRoutes
-from pyramid.config import Configurator
-from pyramid.events import BeforeRender
-from pyramid.events import NewRequest
 from pyramid_beaker import session_factory_from_settings,set_cache_regions_from_settings
+from pyramid.config import Configurator
+from pyramid.events import BeforeRender, NewRequest
+from patchman.utils.subscribers import add_renderer_globals, csrf_validation
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -30,13 +29,12 @@ def main(global_config, **settings):
     config.add_subscriber(add_renderer_globals, BeforeRender)
     config.add_subscriber(csrf_validation, NewRequest)    
     
-    # mako settings for file extension .html
-    config.include('pyramid_mako')
+    
     config.add_mako_renderer(".html")
 
     config.add_static_view("static", "patchman:static", cache_max_age=3600)
     
     MyRoutes(config)
-    
+      
     config.scan()
     return config.make_wsgi_app()
