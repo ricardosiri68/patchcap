8e2f5c9be43a3ae9f1088b1b4b17ca985bf02814
Hernando Rojas <hernando.rojas@gmail.com>
2015-03-10 17:47:55 -0300

    splitted in ffplay and gstplay

--- cam/play.sh
+++
@@ -1,5 +0,0 @@
-#gst-launch rtspsrc location=rtsp://root:root@192.168.3.20:554/cam1/h264 ! decodebin ! autovideosink
-#mjpeg
-#gst-launch -v souphttpsrc location="`sed -n '2p' sources.txt`" do-timestamp=true is_live=true ! multipartdemux ! jpegdec ! ffmpegcolorspace ! autovideosink
-#rtsp
-#gst-launch -v rtspsrc location="`sed -n '2p' sources.txt`" debug=1 ! rtpmp4vdepay ! mpeg4videoparse ! ffdec_mpeg4 ! ffmpegcolorspace! autovideosink
