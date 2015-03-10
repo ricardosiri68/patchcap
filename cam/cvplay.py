2530440f6a9311db5b4afa96f7386529f79630f6
Hernando Rojas <hernando.rojas@gmail.com>
2015-03-10 18:03:00 -0300

    opencv video player

---
+++ cam/cvplay.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+import cv2
+
+uri = 0
+if len(sys.argv)==2:
+    uri = sys.argv[1]
+c = cv2.VideoCapture(uri)
+
+
+while(1):
+    _,f = c.read()
+    k = cv2.waitKey(5)
+    if not _ or k>0:
+        break;
+    cv2.imshow('e2',f)
+cv2.destroyAllWindows()
+
+
