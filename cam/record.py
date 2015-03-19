2254e6fb9769e302c3ff8e670757fe3ce463bdf0
Hernando Rojas <hernando.rojas@gmail.com>
2015-03-19 18:08:25 -0300

    update to opencv 3

--- cam/record.py
+++ cam/record.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 import cv2
-import cv2.cv as cv
 import sys
 
 if len(sys.argv)!=2:
     uri = "rtsp://root:root@192.168.3.20:554/cam1/onvif-h264"
+    output = "out.avi"
 else:
     uri = sys.argv[1]
+    output = sys.argv[2]
 
 
 video = cv2.VideoCapture(uri)
-w = (int)(video.get( cv.CV_CAP_PROP_FRAME_WIDTH ))
-h = (int)(video.get( cv.CV_CAP_PROP_FRAME_HEIGHT ))
-_fps = video.get(cv.CV_CAP_PROP_FPS)
-if not _fps: _fps = 25   
-writer = cv2.VideoWriter(filename="out.avi",  #Provide a file to write the video to
-                         fourcc=cv.CV_FOURCC('X','2', '6', '4'),            #Use whichever codec works for you...
+w = (int)(video.get( cv2.CAP_PROP_FRAME_WIDTH ))
+h = (int)(video.get( cv2.CAP_PROP_FRAME_HEIGHT ))
+_fps = video.get(cv2.CAP_PROP_FPS)
+if not _fps: _fps = 25 
+writer = cv2.VideoWriter(filename="out.avi",  
+                         fourcc=cv.CV_FOURCC('X','2', '6', '4'), 
                          fps = _fps,
                          frameSize=(w, h))
 
