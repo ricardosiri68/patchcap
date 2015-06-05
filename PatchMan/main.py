c9acdacb13e9f1013ba58417760d059d6299ea0a
Hernando Rojas <hernando.rojas@gmail.com>
2015-06-05 17:14:47 -0300

    enable run with multiple devices

--- PatchMan/main.py
+++ PatchMan/main.py
@@ -2,12 +2,24 @@
 import sys
 from patchfinder import PatchFinder
 
-if len(sys.argv) >= 2:
-    i = sys.argv[1]
-else:
-    i = 1
+from optparse import OptionParser
+parser = OptionParser(usage=("%%prog [options] [command]\n"
+                                 "Commands: start, stop, restart and nodaemon.\n"
+                                 ))
+parser.add_option("-D", "--debug",
+                      action="store_true", dest="debug", default=False,
+                      help="render debug to buffer and print messages")
 
-log = len(sys.argv) == 3 and sys.argv[2]
+parser.add_option("-A", "--monitor-all",
+                      action="store_true", dest="all", default=False,
+                      help="Add all enabled devices to be monitored.")
 
-pf = PatchFinder(i)
+parser.add_option("-d", "--device",
+                      action="append",
+                      dest="devices", default=[],
+                      help="Add device to monitor  using id.")
+
+(options, args) = parser.parse_args()
+ 
+pf = PatchFinder(options)
 pf.run()
--- PatchMan/patchfinder.py
+++ PatchMan/patchfinder.py
@@ -26,58 +26,43 @@ GObject.threads_init()
 Gst.init(None)
 
 class MyFactory(GstRtspServer.RTSPMediaFactory):
-    def __init__(self, fmt):
+    def __init__(self, fmt, name):
         self.fmt = fmt
+        self.name = name
         GstRtspServer.RTSPMediaFactory.__init__(self)
         self.set_shared(True)
-        
+
     def do_create_element(self, url):
         if self.fmt == 'mp4':
-            pipe = "( intervideosrc ! videoconvert ! avenc_mpeg4 ! rtpmp4vpay name=pay0 )"
+            pipe = "( intervideosrc channel=%s name=%s ! videoconvert ! avenc_mpeg4 ! rtpmp4vpay name=pay0 )"%(self.name, self.name)
         elif self.fmt =='h264':
-            pipe = "( intervideosrc ! videoconvert ! vaapipostproc ! vaapiencode_h264 ! rtph264pay name=pay0 pt=96 )"
+            pipe = "( intervideosrc channel=%s name=%s ! videoconvert ! vaapipostproc ! vaapiencode_h264 ! rtph264pay name=pay0 pt=96 )"%(self.name, self.name)
         return Gst.parse_launch(pipe)
 
 
 class StreamServer():
-    def __init__(self, name):
+    def __init__(self):
         self.server = GstRtspServer.RTSPServer()
-        m = self.server.get_mount_points()
-        m.add_factory("/{0}/mp4".format(name), MyFactory('mp4'))
-        m.add_factory("/{0}/h264".format(name),MyFactory('h264'))
         self.server.attach(None)
 
+    def add(self, name):
+        m = self.server.get_mount_points()
+        # m.add_factory("/{0}/mp4".format(name), MyFactory('mp4', name))
+        m.add_factory("/{0}/h264".format(name),MyFactory('h264', name))
 
-class PatchFinder(Daemon):
 
-    def __init__(self, device_id):
-        self.env = None
-        super(PatchFinder, self).__init__(
-            "/tmp/patchfinder.pid",
-            stdin='/dev/stdin',
-            stderr='/dev/stderr',
-            stdout='/dev/stdout'
-        )
-
-        self.env = bootstrap(path.dirname(path.realpath(__file__))+'/halcon.ini')
-        initialize_sql(self.env['registry'].settings)
+class Finder(object):
+    def __init__(self, dev):
         self.caps = None
-        self.dev = Device.findBy(device_id)
-        
-        if not self.dev:
-            logger.fatal('No existe el source %s',device_id)
-            sys.exit(3)
-
-        self.mainloop = GObject.MainLoop()
+        self.dev = dev
         self.pipeline = Gst.Pipeline()
-
         self.bus = self.pipeline.get_bus()
         self.bus.add_signal_watch()
         self.bus.connect("message", self.on_message)
-        self.src = VirtualDevice(self.dev.instream)
+        self.src = VirtualDevice(dev.instream)
         self.vc1 = Gst.ElementFactory.make('videoconvert', None)
-        self.video = PlateFinder(self.dev)
-        self.sink = GstOutputStream(self.dev.outstream, split=False)
+        self.video = PlateFinder(dev)
+        self.sink = GstOutputStream(dev.outstream, split=False)
 
         self.pipeline.add(self.src)
         self.pipeline.add(self.vc1)
@@ -87,40 +72,35 @@ class PatchFinder(Daemon):
         self.vc1.link(self.video)
         self.video.link(self.sink)
 
-    def run(self):
-        if logger.isEnabledFor(logging.DEBUG):
-            Gst.debug_bin_to_dot_file_with_ts(self.pipeline, Gst.DebugGraphDetails.ALL, 'halcon')
-        try:
-            self.pipeline.set_state(Gst.State.PLAYING)
-            self.mainloop.run()
-        except KeyboardInterrupt:
-            logger.warning("Cancelando por Ctrl-C")
-            self.kill()
-            raise KeyboardInterrupt
+    def start(self):
+        logging.info("starting %s"%self.dev.name)
+        self.pipeline.set_state(Gst.State.PLAYING)
 
-    def kill(self):
-        self.pipeline.set_state(Gst.State.NULL)
-        self.mainloop.quit()
+    def stop(self):
+        logging.info("stopping %s"%self.dev.name)
+        self.pipeline.set_state(gst.STATE_NULL)
+
+    def restart(self):
         self.stop()
+        self.start()
 
     def on_message(self, bus, message):
         t = message.type
-        if t == Gst.MessageType.ERROR or t == Gst.MessageType.EOS:
-            self.kill()
+        if t == Gst.MessageType.ERROR: 
             err, debug = message.parse_error()
             error = str(err)
             if debug:
                 error += " (%s)"%debug
                 logger.error("monitor '%s' received error; %s"%(self.dev, error))
+        elif t == Gst.MessageType.EOS:
+            logger.warn('EOS')
         elif t == Gst.MessageType.STATE_CHANGED:
             old, state, pending = message.parse_state_changed()
             if state == Gst.State.NULL:
                 logger.info("monitor '%s' main pipeline is stopped"% self.dev.name)
-                self.kill()
             elif state == Gst.State.PLAYING:
                 if message.src == self.pipeline:
-                    logger.info("'%s' cambio de %s a %s. Pending: %s"%(self.dev.name, self.get_state(old), self.get_state(state), self.get_state(pending))) 
-                    s = StreamServer(self.dev.outstream)
+                    logger.info("'%s' cambio de %s a %s."%(self.dev.name, self.get_state(old), self.get_state(state))) 
         elif t == Gst.MessageType.APPLICATION and message.has_name('video/x-raw'):
             s = message.get_structure()
             self.caps = s.to_string()
@@ -136,27 +116,9 @@ class PatchFinder(Daemon):
                 error += " (%s)"%d
                 logger.warn("monitor '%s' received warning; %s"%(self.dev, error))
 
-
-    def log(self, img, code, stats):
-        stats.detected()
-        if self.dev.logging:
-            logger.debug("loging capture to db")
-            transaction.begin()
-            plate = Plate.findBy(code)
-            if plate.active:
-                self.src.alarm()
-            plate.log()
-            transaction.commit()
-        ts=datetime.now().strftime("%Y%m%d-%H%M%S")
-        log.save_image(img,code+ts,'log/')
-        if img.filename:
-            real = path.splitext(path.basename(img.filename))[0].upper()
-            output = code.upper().replace(" ","")[:6]
-            if output == real[:6]:
-                logger.debug("\033[92m" + output + ": OK \033[0m")
-                stats.found()
-            else:
-                logger.debug(real[:6] + ": " + output)
+    def stop(self):
+        self.pipeline.set_state(Gst.State.NULL)
+        self.bus.remove_signal_watch()
 
     def get_state(self, state):
         if state == Gst.State.PLAYING:
@@ -169,10 +131,93 @@ class PatchFinder(Daemon):
             return "pending"
         else:
             return "null"
-
-
+    
     def __del__(self):
-        self.kill()
+        self.stop()
+
+
+class PatchFinder(Daemon):
+
+    def __init__(self, options, pidfile = None):
+        self.env = None
+        self.server = None
+        self.mainloop = GObject.MainLoop()
+        self.finders = []
+        self.options = options
+
+        if self.options.debug:
+            logger.debug(self.options)
+
+        super(PatchFinder, self).__init__(
+           "/tmp/patchfinder.pid",
+            stdin='/dev/stdin',
+            stderr='/dev/stderr',
+            stdout='/dev/stdout'
+        )
+
+    def run(self):
+        self.server = StreamServer()
+        self.env = bootstrap(path.dirname(path.realpath(__file__))+'/halcon.ini')
+        initialize_sql(self.env['registry'].settings)
+        self.finders = self.setup_monitors()
+        try:
+            self.mainloop.run()
+        except KeyboardInterrupt:
+            logger.warning("Cancelando...")
+            self.stop_children()
+            raise KeyboardInterrupt
+        finally:
+            self.mainloop.quit()
+
+    def setup_monitors(self):
+        logger.debug('configurando dispositivos')
+        finders = []
+        if self.options.all:
+            devs = Device.enabled()
+        else:
+            devs = []
+            for o in self.options.devices:
+                devs.append(Device.findBy(int(o)))
+            
+        logger.debug('configurando %s dispositivos',len(devs))
+        for d in devs:
+            f = Finder(d)
+            f.start()
+            finders.append(f)
+            self.server.add(d.outstream)
+        return finders
+
+
+
+    def log(self, img, code, stats):
+        stats.detected()
+        if self.dev.logging:
+            logger.debug("loging capture to db")
+            transaction.begin()
+            plate = Plate.findBy(code)
+            if plate.active:
+                self.src.alarm()
+            plate.log()
+            transaction.commit()
+        ts = datetime.now().strftime("%Y%m%d-%H%M%S")
+        log.save_image(img,code+ts,'log/')
+        if img.filename:
+            real = path.splitext(path.basename(img.filename))[0].upper()
+            output = code.upper().replace(" ","")[:6]
+            if output == real[:6]:
+                logger.debug("\033[92m" + output + ": OK \033[0m")
+                stats.found()
+            else:
+                logger.debug(real[:6] + ": " + output)
+
+
+    def stop_children(self):
+        for m in self.finders:
+            m.stop()
+ 
+    def __del__(self):
+        if self.mainloop and self.mainloop.is_running():
+           self.mainloop.quit()
         self.src = None
         if self.env:
             self.env['closer']()
@@ -181,20 +226,50 @@ class PatchFinder(Daemon):
 
 
 
-
 if __name__ == "__main__": 
-    daemon = PatchFinder(sys.argv[2])
-    if len(sys.argv) >= 2:
-        if 'start' == sys.argv[1]:
-                daemon.start()
-        elif 'stop' == sys.argv[1]:
-                daemon.stop()
-        elif 'restart' == sys.argv[1]:
-                daemon.restart()
-        else:
-            print("comando desconocido")
-        sys.exit(2)
-        sys.exit(0)
+    from optparse import OptionParser
+    parser = OptionParser(usage=("%%prog [options] [command]\n"
+                                 "Commands: start, stop, restart and nodaemon.\n"
+                                 ))
+    parser.add_option("-D", "--debug",
+                      action="store_true", dest="debug", default=False,
+                      help="render debug to buffer and print messages")
+
+    parser.add_option("-A", "--monitor-all",
+                      action="store_true", dest="all", default=False,
+                      help="Add all enabled devices to be monitored.")
+
+    parser.add_option("-d", "--device",
+                      action="append",
+                      dest="devices", default=[],
+                      help="Add device to monitor  using id.")
+
+    (options, args) = parser.parse_args()
+    
+    daemon = PatchFinder(options)
+    
+    if args:
+        arg = args[0].strip().lower()
+        if arg == 'start':
+            logger.debug('start daemon')
+            daemon.start()
+        elif arg == 'stop':
+            daemon.stop()
+        elif arg == 'restart':
+            logger.debug('restart daemon')
+            daemon.restart()
+        elif arg == "nodaemon":
+            print('run no daemon')
+            try:
+                daemon.run()
+            except KeyboardInterrupt:
+                logger.warning("Cancelando...")
     else:
-        print("uso: %s start|stop|restart" % sys.argv[0])
-        sys.exit(2)
+        parser.print_help()
+        print
+        pid = daemon.getpid()
+        if pid:
+            print "daemon is running according to '%s'"%daemon.pidfile
+        else:
+            print "daemonis not running according to '%s'"%daemon.pidfile
+        print
