8e7e53fdb61625ebc200580ce2be27de41561beb
Hernando Rojas <hernando.rojas@gmail.com>
2015-09-21 18:19:26 -0300

    service sample

---
+++ src/test.py
@@ -0,0 +1,34 @@
+#Users
+#list
+curl -XGET -i localhost:8080/api/users
+
+#add
+curl -i -H "Content-Type: application/json" -X POST -d '{"name":"Name Lastname","username":"user", "password":"pepe","email","user@mirich.com.ar"}' localhost:8080/api/users
+
+#get id = 1
+curl -XGET -i localhost:8080/api/users/1
+
+#update
+curl -i -H "Content-Type: application/json" -X PUT -d '{"name":"Name Lastname","username":"user", "password":"pepe2","email","other@mirich.com.ar"}' localhost:8080/api/users
+
+#del
+curl -X DELETE -i localhost:8080/api/users/1
+
+#Cams
+
+#list
+curl -XGET -i localhost:8080/api/devices
+
+#get id=1
+curl -XGET -i localhost:8080/api/devices/1
+
+#add
+curl -i -H "Content-Type: application/json" -X POST -d '{"instream":"rtsp://192.168.2.3:554/cam/realmonitor?channel=1&subtype=0&authbasic=YWRtaW46YWRtaW4=","ip":"192.168.2.3", "outstream":"poste2","name":"hola2"}' localhost:8080/api/devices
+
+#upd el id=5
+curl -i -H "Content-Type: application/json" -X PUT -d '{"instream:"rtsp://192.168.2.3:554/cam/realmonitor?channel=1&subtype=0&authbasic=YWRtaW46YWRtaW4=","ip":"192.168.2.3", "outstream":"poste22","name":"hola2"}' localhost:8080/api/devices/5
+
+#del id=2
+curl -X DELETE -i localhost:8080/api/devices/2
+
+
