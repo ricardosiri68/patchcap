aed1fb91e5f4d6f83aed202108bc49f5ae630dce
Hernando Rojas <hernando.rojas@gmail.com>
2015-10-15 19:40:52 -0300

    get devices from backend

---
+++ PatchMan/client.py
@@ -0,0 +1,27 @@
+import requests
+from ConfigParser import ConfigParser
+
+
+
+
+class   Backend(object):
+
+    def __init__(self):
+        config = ConfigParser()
+        config.read('secret_settings.ini')
+        self.user = config.get('backend', 'user')
+        self.password = config.get('backend', 'password')
+        self.client = requests.Session()
+        auth = {'username': 'hernando', 'password': 'nonsecurepass'}
+        login="http://localhost:8080/api/users/login/"
+        r = self.client.post(login, json=auth)
+        self.url_base ='http://localhost:8080/api/devices'
+
+    def devices(self, id = None):
+        if id:
+            url = self.url_base+'/'+str(id)
+        else:
+            url = self.url_base
+        r = self.client.get(url)
+        return r.json()
+
